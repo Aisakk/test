4d67cdaf90387fa797f0ecc3fcf807c350fd001d
Aisak <santaellaisaac2@gmail.com>
2021-10-13 22:51:18 -0400

    test 30%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  let numberList = [1,2,3,4,5,6,7,8,9];
+  let number = []
+  let result;
+  
+
+  function test(event){
+    if(number.length < 2){
+      number.push(Number(event.target.value))
+    }
+    console.log(number)
+  }
+  useEffect(()=>{
+  
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Select digits to create a Sum</h2>
+      <select onChange={test} defaultValue="Escoja una opcion">
+        <option selected> Escoja una opcion</option>
+        {numberList.map((value,index) => <option key={index} value={value}>{value}</option>)}
+      </select>
+      <h3>Suma:</h3>
+      { number.length < 2 ? <p>0</p> : number.reduce((a,b)=> a+b)}
     </div>
   );
 }
